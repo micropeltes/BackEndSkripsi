86a05597a370c166a59a35302dd7d0b0d72c9df1
micropeltes <kristoforusgwayne@gmail.com>
2026-03-30 18:51:15 +0700

    abuyMBG

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from api import router
 from mqttsub import start_mqtt
@@ -10,11 +11,22 @@ app.include_router(router)
 
 @app.on_event("startup")
 def startup():
-
     print("Starting backend...")
-
-    # cek database
-    save_data()
-
-    # start MQTT subscriber
     start_mqtt()
+    # cek databasefrom contextlib import asynccontextmanager
+from fastapi import FastAPI
+from api import router
+from mqttsub import start_mqtt
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Starting backend...")
+    start_mqtt()  # jalankan MQTT saat startup
+    yield
+    print("Shutting down backend...")
+
+app = FastAPI(lifespan=lifespan)
+
+app.include_router(router)
+    # save_data(value)
+    # start MQTT subscriber
--- mqttsub.py
+++ mqttsub.py
@@ -1,21 +1,49 @@
+import json
 import paho.mqtt.client as mqtt
 from database import save_data
 
-BROKER = "broker.emqx.io"
-TOPIC = "sensor/control"
+BROKER = "45.126.43.35"
+PORT = 8883
+TOPIC = "test/topic"
+
+CA_CERT = r"C:\mqtt\ca.crt"
 
 def on_connect(client, userdata, flags, rc):
-    print("Connected to MQTT")
+    print("Connected to MQTT with code:", rc)
     client.subscribe(TOPIC)
 
 def on_message(client, userdata, msg):
-    payload = msg.payload.decode()
-    print("Received:", payload)
-    save_data(payload)
+    try:
+        payload = msg.payload.decode()
+        print("Raw:", payload)
+
+        data = json.loads(payload)  # 🔥 parsing JSON
+
+        # Ambil field
+        devid = data.get("DevID")
+        nh3_mics = data.get("NH3_MICS")
+        nh3_mems = data.get("NH3_MEMS")
+        h2s = data.get("H2S")
+        no2 = data.get("NO2")
+        co = data.get("CO")
+        mq135 = data.get("MQ135")
+
+        print("Parsed:", data)
+
+        # Simpan ke DB
+        save_data(devid, nh3_mics, nh3_mems, h2s, no2, co, mq135)
+
+    except Exception as e:
+        print("Error parsing message:", e)
 
 def start_mqtt():
     client = mqtt.Client()
+
     client.on_connect = on_connect
     client.on_message = on_message
-    client.connect(BROKER, 1883, 60)
+
+    client.tls_set(ca_certs=CA_CERT)
+    client.tls_insecure_set(True)
+
+    client.connect(BROKER, PORT, 60)
     client.loop_start()
